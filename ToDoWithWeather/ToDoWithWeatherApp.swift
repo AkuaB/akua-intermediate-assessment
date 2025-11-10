25e43dcd9b496867117017ee338f4695c6e5ea03
AkuaB <u15019773@tuks.co.za>
2025-11-11 01:01:29 +0200

    Initial Commit

---
+++ ToDoWithWeather/ToDoWithWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoWithWeatherApp.swift
+//  ToDoWithWeather
+//
+//  Created by Akua Afrane-Okese on 2025/11/11.
+//
+
+import SwiftUI
+
+@main
+struct ToDoWithWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
